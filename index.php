032f2f188325994ce9ea1a17eff49be257b02c47
sparetype <griffin@sparetype.com>
2013-12-03 03:57:00 +0000

    Body background image

--- index.php
+++ index.php
@@ -107,7 +107,6 @@ else
 		body.site
 		{
 			border-top: 3px solid <?php echo $this->params->get('templateColor');?>;
-			background-color: <?php echo $this->params->get('templateBackgroundColor');?>
 		}
 		a
 		{
