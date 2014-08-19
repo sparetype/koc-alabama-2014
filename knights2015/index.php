2917a80f9da3e998729d446960f79251a7fb7875
sparetype <griffin@sparetype.com>
2014-08-19 21:50:57 +0000

    Rebuilt project for development

---
+++ knights2015/index.php
@@ -0,0 +1,43 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<jdoc:include type="head" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+</head>
+
+<body class="body">
+
+	<jdoc:include type="message" />
+
+	<header class="header">
+	<jdoc:include type="modules" name="header" />
+	</header>
+	
+	<div class="container">
+	
+	<div class="breadcrumb" role="menu">
+	<jdoc:include type="modules" name="breadcrumb" />
+	</div>
+
+	<div class="content">
+	<jdoc:include type="component" />
+	</div>
+
+	<div class="right-sidebar">
+	<jdoc:include type="modules" name="right" />
+	</div>
+	
+	</div> <!-- end container -->
+
+	<div class="left-sidebar">
+	<jdoc:include type="modules" name="left" />
+	</div>
+	
+	<footer class="footer">
+	<jdoc:include type="modules" name="footer" />
+	</footer>
+</body>
+</html>
