ce4e50ceb8fa1e295aa3c25a6527699b1647f39a
sparetype <griffin@sparetype.com>
2014-08-20 18:49:58 +0000

    Grunt watch trial

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,46 @@
 module.exports = function(grunt) {
-// Project configuration.
-grunt.initConfig({
-pkg: grunt.file.readJSON('package.json'),
-concat: {
-dist: {
-src: ['src/js/*.js' // All JS in the src folder
-],
-dest: 'src/js/production.js',
-}
-},
-uglify: {
-build: {
-src: 'src/js/production.js',
-dest: 'knights2015/scripts/production.min.js'
-}
-},
-sass: {
-build: {
-options: {
-style: 'compressed'
-},
-files: {
-'knights2015/css/template.css': 'src/sass/template.scss'
-}
-}
-}
-});
-// Load the plugin that provides the "uglify" task.
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-sass');
-// Default tasks.
-grunt.registerTask('default', ['concat', 'uglify', 'sass']);
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: ['src/js/*.js' // All JS in the src folder
+                ],
+                dest: 'src/js/production.js',
+            }
+        },
+        uglify: {
+            build: {
+                src: 'src/js/production.js',
+                dest: 'knights2015/scripts/production.min.js'
+            }
+        },
+        sass: {
+            build: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'knights2015/css/template.css': 'src/sass/template.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['src/sass/*.scss'],
+                tasks: ['sass'],
+                options: {
+                  spawn: false,
+			      debounceDelay: 10000,
+                }
+            }
+        }
+    });
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // Default tasks.
+    grunt.registerTask('default', ['concat', 'uglify', 'sass']);
 };
